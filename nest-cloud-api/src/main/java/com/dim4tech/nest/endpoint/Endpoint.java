c4171a810d6cc3239032d2b45844675e8aeb7d13
developer <dimer1@mail.ru>
2016-05-17 17:12:27 +0300

    Endpoints hierarchy was added

--- nest-cloud-api/src/main/java/com/dim4tech/nest/endpoint/Endpoint.java
+++ nest-cloud-api/src/main/java/com/dim4tech/nest/endpoint/Endpoint.java
@@ -3,15 +3,28 @@ package com.dim4tech.nest.endpoint;
 public enum Endpoint {
     ROOT(""),
     DEVICES("devices"),
+    DEVICES_THERMOSTATS(DEVICES, "thermostats"),
+    DEVICES_SMOKE_CO_ALARMS(DEVICES, "smoke_co_alarms"),
+    DEVICES_CAMERAS(DEVICES, "cameras"),
     STRUCTURES("structures");
 
+    private final Endpoint parentEndpoint;
     private final String path;
 
     Endpoint(String path) {
+        this(null, path);
+    }
+
+    Endpoint(Endpoint parentEndpoint, String path) {
+        this.parentEndpoint = parentEndpoint;
         this.path = path;
     }
 
     public String getPath() {
-        return path;
+        StringBuilder pathBuilder = new StringBuilder();
+        if (this.parentEndpoint != null) {
+            pathBuilder.append(this.getPath()).append("/");
+        }
+        return pathBuilder.append(path).toString();
     }
 }
