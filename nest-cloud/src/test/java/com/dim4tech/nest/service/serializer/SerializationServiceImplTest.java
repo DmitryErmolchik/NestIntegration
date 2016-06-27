829167b8d546ae0024d31c28c5039704e4dc2fb6
developer <dimer1@mail.ru>
2016-06-27 10:16:06 +0300

    Test was updated

--- nest-cloud/src/test/java/com/dim4tech/nest/service/serializer/SerializationServiceImplTest.java
+++ nest-cloud/src/test/java/com/dim4tech/nest/service/serializer/SerializationServiceImplTest.java
@@ -1,10 +1,15 @@
 package com.dim4tech.nest.service.serializer;
 
+import com.dim4tech.nest.domain.payload.ProductData;
+import com.dim4tech.nest.domain.payload.Software;
 import com.dim4tech.nest.domain.payload.Thermostat;
 import com.dim4tech.nest.domain.payload.constant.HvacMode;
+import com.dim4tech.nest.helper.writebuilder.ProductDataBuilder;
+import com.dim4tech.nest.helper.writebuilder.ProductResourceBuilder;
 import com.dim4tech.nest.helper.writebuilder.ThermostatBuilder;
 import com.dim4tech.nest.service.deserializer.DeserializationService;
 import com.dim4tech.nest.service.deserializer.DeserializationServiceImpl;
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -23,4 +28,14 @@ public class SerializationServiceImplTest {
         assertEquals(thermostat, deserializationService.deserialize(serializedThermostat, Thermostat.class));
     }
 
+    @Test
+    public void productDataSerialize() throws Exception {
+        ProductData productData = new ProductDataBuilder().setSoftware(new Software("1.0"))
+                .addResourceUse("electricity", new ProductResourceBuilder().setValue(42.789).setMeasurementResetTime(DateTime.parse("2016-01-01T01:01:01.000Z")).setMeasurementTime(DateTime.parse("2016-01-01T01:02:35.000Z")).build())
+                .addResourceUse("gas", new ProductResourceBuilder().setValue(0.345234545).setMeasurementResetTime(DateTime.parse("2016-01-01T01:01:01.000Z")).setMeasurementTime(DateTime.parse("2016-01-01T01:02:35.000Z")).build())
+                .addResourceUse("water", new ProductResourceBuilder().setValue(10000.3).setMeasurementResetTime(DateTime.parse("2016-01-01T01:01:01.000Z")).setMeasurementTime(DateTime.parse("2016-01-01T01:02:35.000Z")).build())
+                .build();
+        String serializedProductData = serializationService.serialize(productData);
+        assertEquals(productData, deserializationService.deserialize(serializedProductData, ProductData.class));
+    }
 }
