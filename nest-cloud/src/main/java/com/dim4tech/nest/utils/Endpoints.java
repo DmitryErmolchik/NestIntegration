433411b215d0db2f3a5f7b84826df167879319d4
developer <dimer1@mail.ru>
2016-06-27 11:33:16 +0300

    Endpoints class was updated

--- nest-cloud/src/main/java/com/dim4tech/nest/utils/Endpoints.java
+++ nest-cloud/src/main/java/com/dim4tech/nest/utils/Endpoints.java
@@ -1,6 +1,7 @@
 package com.dim4tech.nest.utils;
 
 import com.dim4tech.nest.domain.payload.DeviceId;
+import com.dim4tech.nest.domain.payload.ProductTypeId;
 import com.dim4tech.nest.domain.payload.StructureId;
 import com.dim4tech.nest.endpoint.Endpoint;
 import com.dim4tech.nest.exception.NestIntegrationException;
@@ -15,67 +16,79 @@ public class Endpoints {
         this.rootPath = rootPath;
     }
 
-    public URL getNestData() {
+    public URL getNestDataUrl() {
         return rootPath;
     }
 
-    public URL getDevices() {
+    public URL getDevicesUrl() {
         return buildEndpoint(Endpoint.DEVICES);
     }
 
-    public URL getThermostats() {
+    public URL getThermostatsUrl() {
         return buildEndpoint(Endpoint.DEVICES_THERMOSTATS);
     }
 
-    public URL getThermostat(String thermostatId) {
+    public URL getThermostatUrl(String thermostatId) {
         return buildEndpoint(Endpoint.DEVICES_THERMOSTATS, thermostatId);
     }
 
-    public URL getThermostat(DeviceId thermostatId) {
-        return getThermostat(thermostatId.getId());
+    public URL getThermostatUrl(DeviceId thermostatId) {
+        return getThermostatUrl(thermostatId.getId());
     }
 
-    public URL getSmokeCoAlarms() {
+    public URL getSmokeCoAlarmsUrl() {
         return buildEndpoint(Endpoint.DEVICES_SMOKE_CO_ALARMS);
     }
 
-    public URL getSmokeCoAlarm(String smokeCoAlarmId) {
+    public URL getSmokeCoAlarmUrl(String smokeCoAlarmId) {
         return buildEndpoint(Endpoint.DEVICES_SMOKE_CO_ALARMS, smokeCoAlarmId);
     }
 
-    public URL getSmokeCoAlarm(DeviceId smokeCoAlarmId) {
-        return getSmokeCoAlarm(smokeCoAlarmId.getId());
+    public URL getSmokeCoAlarmUrl(DeviceId smokeCoAlarmId) {
+        return getSmokeCoAlarmUrl(smokeCoAlarmId.getId());
     }
 
-    public URL getCameras() {
+    public URL getCamerasUrl() {
         return buildEndpoint(Endpoint.DEVICES_CAMERAS);
     }
 
-    public URL getCamera(String cameraId) {
+    public URL getCameraUrl(String cameraId) {
         return buildEndpoint(Endpoint.DEVICES_CAMERAS, cameraId);
     }
 
-    public URL getCamera(DeviceId cameraId) {
-        return getCamera(cameraId.getId());
+    public URL getCameraUrl(DeviceId cameraId) {
+        return getCameraUrl(cameraId.getId());
     }
 
-    public URL getCompany(String company) {
+    public URL getCompanyUrl(String company) {
         return buildEndpoint(Endpoint.DEVICES, company);
     }
 
-    public URL getStructures() {
+    public URL getCompanyProductTypesUrl(String company, String productType) {
+        return buildEndpoint(Endpoint.DEVICES, company, productType);
+    }
+
+    public URL getCompanyProductDataUrl(String company, String productType, String productTypeId) {
+        return buildEndpoint(Endpoint.DEVICES, company, productType, productTypeId);
+    }
+
+    public URL getCompanyProductDataUrl(String company, String productType, ProductTypeId productTypeId) {
+        return getCompanyProductDataUrl(company, productType, productTypeId.getId());
+    }
+
+    public URL getStructuresUrl() {
         return buildEndpoint(Endpoint.STRUCTURES);
     }
 
-    public URL getStructure(String structureId) {
+    public URL getStructureUrl(String structureId) {
         return buildEndpoint(Endpoint.STRUCTURES, structureId);
     }
 
-    public URL getStructure(StructureId structureId) {
-        return getStructure(structureId.getId());
+    public URL getStructureUrl(StructureId structureId) {
+        return getStructureUrl(structureId.getId());
     }
 
-    public URL getMetadata() {
+    public URL getMetadataUrl() {
         return buildEndpoint(Endpoint.METADATA);
     }
 
@@ -88,9 +101,16 @@ public class Endpoints {
         }
     }
 
-    private URL buildEndpoint(Endpoint parentEndpoint, String customEndpoint) {
+    private URL buildEndpoint(Endpoint parentEndpoint, String... customEndpoints) {
+        StringBuilder customEndpointPathBuilder = new StringBuilder();
+        for (int i=0; i< customEndpoints.length; i++) {
+            customEndpointPathBuilder.append(customEndpoints[i]);
+            if (i < customEndpoints.length-1) {
+                customEndpointPathBuilder.append("/");
+            }
+        }
         try {
-            return new URL(rootPath, parentEndpoint.getPath() + "/" + customEndpoint);
+            return new URL(rootPath, parentEndpoint.getPath() + "/" + customEndpointPathBuilder.toString());
         } catch (MalformedURLException e) {
             e.printStackTrace();
             throw new NestIntegrationException();
--- nest-cloud/src/test/java/com/dim4tech/nest/utils/EndpointsTest.java
+++ nest-cloud/src/test/java/com/dim4tech/nest/utils/EndpointsTest.java
@@ -1,6 +1,7 @@
 package com.dim4tech.nest.utils;
 
 import com.dim4tech.nest.domain.payload.DeviceId;
+import com.dim4tech.nest.domain.payload.ProductTypeId;
 import com.dim4tech.nest.domain.payload.StructureId;
 import org.junit.Test;
 
@@ -19,61 +20,71 @@ public class EndpointsTest {
 
     @Test
     public void getNestData() throws Exception {
-        assertEquals(baseUrl, endpoints.getNestData().toString());
+        assertEquals(baseUrl, endpoints.getNestDataUrl().toString());
     }
 
     @Test
     public void getDevices() throws Exception {
-        assertEquals(baseUrl + "devices", endpoints.getDevices().toString());
+        assertEquals(baseUrl + "devices", endpoints.getDevicesUrl().toString());
     }
 
     @Test
     public void getThermostats() throws Exception {
-        assertEquals(baseUrl + "devices/thermostats", endpoints.getThermostats().toString());
+        assertEquals(baseUrl + "devices/thermostats", endpoints.getThermostatsUrl().toString());
     }
 
     @Test
     public void getThermostat() throws Exception {
-        assertEquals(baseUrl + "devices/thermostats/123", endpoints.getThermostat(new DeviceId("123")).toString());
+        assertEquals(baseUrl + "devices/thermostats/123", endpoints.getThermostatUrl(new DeviceId("123")).toString());
     }
 
     @Test
     public void getSmokeCoAlarms() throws Exception {
-        assertEquals(baseUrl + "devices/smoke_co_alarms", endpoints.getSmokeCoAlarms().toString());
+        assertEquals(baseUrl + "devices/smoke_co_alarms", endpoints.getSmokeCoAlarmsUrl().toString());
     }
 
     @Test
     public void getSmokeCoAlarm() throws Exception {
-        assertEquals(baseUrl + "devices/smoke_co_alarms/123", endpoints.getSmokeCoAlarm(new DeviceId("123")).toString());
+        assertEquals(baseUrl + "devices/smoke_co_alarms/123", endpoints.getSmokeCoAlarmUrl(new DeviceId("123")).toString());
     }
 
     @Test
     public void getCameras() throws Exception {
-        assertEquals(baseUrl + "devices/cameras", endpoints.getCameras().toString());
+        assertEquals(baseUrl + "devices/cameras", endpoints.getCamerasUrl().toString());
     }
 
     @Test
     public void getCamera() throws Exception {
-        assertEquals(baseUrl + "devices/cameras/123", endpoints.getCamera(new DeviceId("123")).toString());
+        assertEquals(baseUrl + "devices/cameras/123", endpoints.getCameraUrl(new DeviceId("123")).toString());
     }
 
     @Test
     public void getCompany() throws Exception {
-        assertEquals(baseUrl + "devices/$company", endpoints.getCompany("$company").toString());
+        assertEquals(baseUrl + "devices/$company", endpoints.getCompanyUrl("$company").toString());
+    }
+
+    @Test
+    public void getCompanyProductTypesUrl() throws Exception {
+        assertEquals(baseUrl + "devices/$company/$product_type", endpoints.getCompanyProductTypesUrl("$company", "$product_type").toString());
+    }
+
+    @Test
+    public void getCompanyProductDataUrl() throws Exception {
+        assertEquals(baseUrl + "devices/$company/$product_type/123", endpoints.getCompanyProductDataUrl("$company", "$product_type", new ProductTypeId("123")).toString());
     }
 
     @Test
     public void getStructures() throws Exception {
-        assertEquals(baseUrl + "structures", endpoints.getStructures().toString());
+        assertEquals(baseUrl + "structures", endpoints.getStructuresUrl().toString());
     }
 
     @Test
     public void getStructure() throws Exception {
-        assertEquals(baseUrl + "structures/123", endpoints.getStructure(new StructureId("123")).toString());
+        assertEquals(baseUrl + "structures/123", endpoints.getStructureUrl(new StructureId("123")).toString());
     }
 
     @Test
     public void getMetadata() throws Exception {
-        assertEquals(baseUrl + "metadata", endpoints.getMetadata().toString());
+        assertEquals(baseUrl + "metadata", endpoints.getMetadataUrl().toString());
     }
 }
